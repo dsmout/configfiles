6848a4521e9baccc9731982c7d39b6bcdae242b0
Dan Smout <dj.smout@gmail.com>
2021-06-20 23:41:10 +0100

    latest changes

---
+++ spectrwm/lemon.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# ./date.sh | lemonbar -p -g 1920x30+0+0 &
+# ./date.sh | lemonbar -p -g 1920x30+1920+0 &
+
+i3status | lemonbar -p -g 1920x30+0+0 &
+i3status | lemonbar -p -g 1920x30+1920+0 &
+
