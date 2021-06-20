655c67b390a0c3b13d56bb02315f0b87bbe92572
Dan Smout <dj.smout@gmail.com>
2021-06-20 22:48:28 +0100

    Latest changes

---
+++ spectrwm/autostart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# xrandr -s 1920x1080
+# sleep 1
+# feh --bg-scale ~/.config/i3/Wallpapers/harrys.jpg &
+feh --bg-scale ~/.config/leftwm/themes/basic_polybar/Dutch.jpg &
+
+/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
+
