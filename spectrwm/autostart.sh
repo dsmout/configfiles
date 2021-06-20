9df027e99998c73a63cf46228ef8ee949badd40d
Dan Smout <dj.smout@gmail.com>
2021-06-21 00:29:40 +0100

    latest changes

--- spectrwm/autostart.sh
+++ spectrwm/autostart.sh
@@ -3,6 +3,7 @@
 # sleep 1
 # feh --bg-scale ~/.config/i3/Wallpapers/harrys.jpg &
 feh --bg-scale ~/.config/leftwm/themes/basic_polybar/Dutch.jpg &
-
+parcellite -n &
+# greenclip daemon
 /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
 
