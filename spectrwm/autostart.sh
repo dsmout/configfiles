8fc370714f918d0cc56483a05ccfd17fc7af3f0b
Dan Smout <dj.smout@gmail.com>
2021-06-22 23:37:14 +0100

    swapped disable caps lock cmd to autostart file

--- spectrwm/autostart.sh
+++ spectrwm/autostart.sh
@@ -3,7 +3,11 @@
 # sleep 1
 # feh --bg-scale ~/.config/i3/Wallpapers/harrys.jpg &
 feh --bg-scale ~/.config/leftwm/themes/basic_polybar/Dutch.jpg &
+# parcellite for clipboard
 parcellite -n &
 # greenclip daemon
+greenclip daemon &
+# Starting polkit for auth dialogs
 /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
-
+# Disable Caps Lock
+setxkbmap -option caps:none
